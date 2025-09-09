b6cc13174423f4e53155880c6fe85621e95ed48b
Gowtham-beep <gn86923@gmail.com>
2025-09-09 14:46:10 +0530

    Fix: remove unnecessary field checks in addproject function

--- src/controllers/admin/project.js
+++ src/controllers/admin/project.js
@@ -8,7 +8,7 @@ import { updateCloudinaryFiles } from "../../utils/updateCloudinaryFiles.js";
 
 const addproject=async(req,res)=>{
     const{title,description,location,type,technologyused,status,department,designingSoftware,otherType}=req.body
-    if(!title||!description||!location||!type||!technologyused||!status||!department||!designingSoftware||!otherType){
+    if(!title||!description||!location||!type||!technologyused||!status||!department){
         return res.status(400).json({
             message:"Feilds are empty",
             success:false,
